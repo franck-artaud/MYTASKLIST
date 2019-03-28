70615dd5701607fa6f288cd5b7fe9e5081a0d233
atelier601 <franck-artaud@atelier601.com>
2019-03-28 22:05:56 +0100

    bouton PDF

--- app.js
+++ app.js
@@ -76,7 +76,7 @@ function taskAdding(e) {
     //Supprimer une tache
     function killTask(e){
         if (e.target.parentElement.classList.contains('delete-item')){
-            if(confirm('Attention !!! Tu souhaites vraiment suprimer cet élément de ta liste de course ?'))
+            if(confirm('Fait gaffe;; tu vas vraiment supprimer un article de la liste !!!!'))
                 e.target.parentElement.parentElement.remove();
         }
     }
@@ -103,4 +103,27 @@ function taskAdding(e) {
             );
         console.log(contentKeybord);
     }
+
+    //  Exporter la liste au format PDF
+
+    var cache_width = $('#liste').width(); //Criado um cache do CSS
+    var a4 = [595.28, 841.89]; // Widht e Height de uma folha a4
+
+    $(document).on("click", '#btnPrint', function () {
+        // Setar o width da div no formato a4
+        $("#liste").width((a4[0] * 1.33333) - 80).css('max-width', 'none');
+
+        // Aqui ele cria a imagem e cria o pdf
+        html2canvas($('#liste'), {
+            onrendered: function (canvas) {
+                var img = canvas.toDataURL("image/png", 1.0);
+                var doc = new jsPDF({ unit: 'px', format: 'a4' });
+                doc.addImage(img, 'JPEG', 20, 20);
+                doc.save('Ma liste de course de la MORKITU.pdf');
+                //Retorna ao CSS normal
+                $('#liste').width(cache_width);
+            }
+        });
+    }); 
+
 
